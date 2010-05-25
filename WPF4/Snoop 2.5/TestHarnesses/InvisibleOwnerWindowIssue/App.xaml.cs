567570a8664fa49df3df1fd074dfe97100c8b157
cplotts <none@none>
2010-05-25 18:46:48 +0000

    Checking in the InvisibleOwnerWindowsIssue test harness.
    This test harness tests the issue where Snoop previously was unable to Snoop an application because the Application.Current.MainWindow was not visible.
    An exception was thrown when trying to set this window as the owner for the main Snoop UI.
    
    --HG--
    extra : convert_revision : svn%3Aa83551a4-30f6-4d81-a974-c6ced450ddbf%4046558

---
+++ WPF4/Snoop 2.5/TestHarnesses/InvisibleOwnerWindowIssue/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace InvisibleOwnerWindowIssue
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private Window _hiddenWindow;
+		private Window _shownWindow;
+
+		protected override void OnStartup(StartupEventArgs args)
+		{
+			base.OnStartup(args);
+
+			_hiddenWindow = new Window();
+			_hiddenWindow.Title = "Hidden";
+
+			_shownWindow = new Window();
+			_shownWindow.Title = "Shown";
+			_shownWindow.Closing += (sender, e) =>
+			{
+				_hiddenWindow.Close();
+			};
+			_shownWindow.Show();
+		}
+	}
+}
