421d5e1067934c0d9bdc2a61a29824bb6527df55
cplotts <none@none>
2010-05-26 14:36:31 +0000

    (Merged from the WPF35 branch.) Added a simple visual to magnify in order to work on the invisible owner window issue for the magnify part of Snoop.
    --HG--
    extra : convert_revision : svn%3Aa83551a4-30f6-4d81-a974-c6ced450ddbf%4046636

--- WPF4/Snoop 2.5/TestHarnesses/InvisibleOwnerWindowIssue/App.xaml.cs
+++ WPF4/Snoop 2.5/TestHarnesses/InvisibleOwnerWindowIssue/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace InvisibleOwnerWindowIssue
 {
@@ -23,7 +25,16 @@ namespace InvisibleOwnerWindowIssue
 			_hiddenWindow.Title = "Hidden";
 
 			_shownWindow = new Window();
+			_shownWindow.Width = 300;
+			_shownWindow.Height = 300;
 			_shownWindow.Title = "Shown";
+			Rectangle rectangle = new Rectangle();
+			rectangle.Width = 200;
+			rectangle.Height = 50;
+			rectangle.HorizontalAlignment = HorizontalAlignment.Center;
+			rectangle.VerticalAlignment = VerticalAlignment.Center;
+			rectangle.Fill = new SolidColorBrush(Colors.DarkRed);
+			_shownWindow.Content = rectangle;
 			_shownWindow.Closing += (sender, e) =>
 			{
 				_hiddenWindow.Close();
