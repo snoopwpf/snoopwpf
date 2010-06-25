5d863915e4f8a64bc45b0274590df0e1c757e6b6
cplotts <none@none>
2010-06-25 15:40:38 +0000

    Updating the version on the Snoop executable as well.
    --HG--
    extra : convert_revision : svn%3Aa83551a4-30f6-4d81-a974-c6ced450ddbf%4048473

--- 2.6/Snoop/Snoop/Properties/AssemblyInfo.cs
+++ 2.6/Snoop/Snoop/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.5.1.0")]
-[assembly: AssemblyFileVersion("2.5.1.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
