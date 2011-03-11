36e85d856035436a62db8c34e9543ede606625d4
cplotts <none@none>
2011-03-11 17:00:15 +0000

    Updating the logo project to WPF 4.0 ...
    --HG--
    extra : convert_revision : svn%3Aa83551a4-30f6-4d81-a974-c6ced450ddbf%4063642

---
+++ SnoopLogo/SnoopLogo/CodePlexLogoWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnoopLogo
+{
+	/// <summary>
+	/// Interaction logic for CodePlexLogoWindow.xaml
+	/// </summary>
+	public partial class CodePlexLogoWindow : Window
+	{
+		public CodePlexLogoWindow()
+		{
+			this.InitializeComponent();
+
+			// Insert code required on object creation below this point.
+		}
+	}
+}
