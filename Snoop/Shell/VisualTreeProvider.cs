7207397a3466add961da2dde257ae26f42a93a55
bling <bling@live.ca>
2012-07-27 22:50:45 -0400

    add copyright

--- Snoop/Shell/VisualTreeProvider.cs
+++ Snoop/Shell/VisualTreeProvider.cs
@@ -1,3 +1,8 @@
+// (c) Copyright Bailey Ling.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
