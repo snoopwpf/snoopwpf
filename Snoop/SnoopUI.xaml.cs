766b7ba70704a7c4640239d4888835fa4ee9a33c
cplotts <coryplotts@gmail.com>
2012-08-19 07:04:14 -0500

    Rolling back Maciek's changes.
    Rolling back Maciek's changes so that we can move this effort to a task
    branch.

--- Snoop/SnoopUI.xaml.cs
+++ Snoop/SnoopUI.xaml.cs
@@ -452,15 +452,12 @@ namespace Snoop
 			}
 
 			SnoopPartsRegistry.AddSnoopVisualTreeRoot(this);
-            this.Dispatcher.UnhandledException += new DispatcherUnhandledExceptionEventHandler(d_UnhandledException);
+
 			Show();
 			Activate();
 		}
 		public void Inspect(object root, Window ownerWindow)
 		{
-            this.Dispatcher.UnhandledException += new DispatcherUnhandledExceptionEventHandler(d_UnhandledException);
-            ownerWindow.Closing += new CancelEventHandler(ownerWindow_Closing);
-
 			Load(root);
 
 			if (ownerWindow != null)
@@ -476,18 +473,6 @@ namespace Snoop
 			Show();
 			Activate();
 		}
-
-        void ownerWindow_Closing(object sender, CancelEventArgs e)
-        {
-            InputManager.Current.PreProcessInput -= this.HandlePreProcessInput;
-            //throw new NotImplementedException();
-        }
-
-        void d_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            //Should we check if the exception came from Snoop? Perhaps seeing if any Snoop call is in the stack trace?
-            e.Handled = true;
-        }
 
 		public void ApplyReduceDepthFilter(VisualTreeItem newRoot)
 		{
