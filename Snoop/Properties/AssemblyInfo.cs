3dfdaf07d862b9b0b673b59bf953eecce45f8b47
cplotts <coryplotts@gmail.com>
2012-10-03 17:18:56 -0500

    Updated versions for the 2.8.0 release. Added missing copyright info.

--- Snoop/Properties/AssemblyInfo.cs
+++ Snoop/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.7.1.0")]
-[assembly: AssemblyFileVersion("2.7.1.0")]
+[assembly: AssemblyVersion("2.8.0.0")]
+[assembly: AssemblyFileVersion("2.8.0.0")]
