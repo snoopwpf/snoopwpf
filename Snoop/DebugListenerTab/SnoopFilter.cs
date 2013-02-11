7d47d66d82088ddbb0fa9724527b7da126b8a527
Maciek Rakowski <maciekmrakowski@yahoo.com>
2013-02-10 16:45:21 -0800

    Split classes into files

---
+++ Snoop/DebugListenerTab/SnoopFilter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Snoop.DebugListenerTab
+{
+    [Serializable]
+    public abstract class SnoopFilter : INotifyPropertyChanged
+    {
+        protected bool _isGrouped = false;
+        protected string _groupId = string.Empty;
+
+        public abstract bool FilterMatches(string debugLine);
+
+        public virtual bool SupportsGrouping
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool IsGrouped
+        {
+            get
+            {
+                return _isGrouped;
+            }
+            set
+            {
+                _isGrouped = value;
+                this.RaisePropertyChanged("IsGrouped");
+                GroupId = string.Empty;
+            }
+        }
+
+        public virtual string GroupId
+        {
+            get
+            {
+                return _groupId;
+            }
+            set
+            {
+                _groupId = value;
+                this.RaisePropertyChanged("GroupId");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Snoop/DebugListenerTab/SnoopSingleFilter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Snoop.DebugListenerTab
+{
+    [Serializable]
+    public class SnoopSingleFilter : SnoopFilter, ICloneable
+    {
+        private string _text;
+
+        public SnoopSingleFilter()
+        {
+            this.Text = string.Empty;
+        }
+
+        public FilterType FilterType { get; set; }
+
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                this.RaisePropertyChanged("Text");
+            }
+        }
+
+        public override bool FilterMatches(string debugLine)
+        {
+            debugLine = debugLine.ToLower();
+            var text = Text.ToLower();
+            switch (FilterType)
+            {
+                case DebugListenerTab.FilterType.Contains:
+                    return debugLine.Contains(text);
+                case DebugListenerTab.FilterType.StartsWith:
+                    return debugLine.StartsWith(text);
+                case DebugListenerTab.FilterType.EndsWith:
+                    return debugLine.EndsWith(text);
+                case DebugListenerTab.FilterType.RegularExpression:
+                    return Regex.IsMatch(debugLine, text);
+            }
+            return false;
+        }
+
+        public object Clone()
+        {
+            SnoopSingleFilter newFilter = new SnoopSingleFilter();
+            newFilter._groupId = this._groupId;
+            newFilter._isGrouped = this._isGrouped;
+            newFilter._text = this._text;
+            newFilter.FilterType = this.FilterType;
+
+            return newFilter;
+        }
+    }
+}
