944201e73ec083633d35f59c7368f540c4af964d
Maciek <maciekmrakowski@yahoo.com>
2013-03-06 12:05:05 -0800

    Update Settings.Designer.cs
    Set the default behavior to clear after delve.

--- Snoop/Properties/Settings.Designer.cs
+++ Snoop/Properties/Settings.Designer.cs
@@ -199,7 +199,7 @@ namespace Snoop.Properties
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ClearAfterDelve
         {
             get
