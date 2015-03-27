7e63051368ed390a620f7a1505d9a1d26ff75ae3
unknown <maciekmrakowski@yahoo.com>
2015-03-26 23:19:38 -0700

    It crashes on sort.

--- Snoop/PropertyGrid2.xaml.cs
+++ Snoop/PropertyGrid2.xaml.cs
@@ -238,8 +238,14 @@ namespace Snoop
 			GridViewColumnHeader headerClicked = (GridViewColumnHeader)args.OriginalSource;
 
 			direction = GetNewSortDirection(headerClicked);
+            if (headerClicked.Column == null)
+                return;
 
-			switch (((TextBlock)headerClicked.Column.Header).Text)
+            var columnHeader = headerClicked.Column.Header as TextBlock;
+            if (columnHeader == null)
+                return;
+
+            switch (columnHeader.Text)
 			{
 				case "Name":
 					this.Sort(PropertyGrid2.CompareNames, direction);
