b76cc5437ba3609b4bb31ca03abc29f27737cefb
YanBingxin <775127481@qq.com>
2017-06-23 15:21:25 +0800

    Add ControlTemplate src Viewer
    Show you the selection's controltemplate src.The function is easy to
    code, but it's very helpful and friendly to the WPF Beginner. I hope
    that our Snoop is more convenient and helps more people,(forgive my poor
    English,Thanks)

---
+++ Snoop/Converters/ControlTemplateToXamlConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Xml;
+using System.Windows.Markup;
+using System.Windows.Data;
+
+namespace Snoop.Converters
+{
+    public class ControlTemplateToXamlConverter : IValueConverter
+    {
+        #region IValueConverter 成员
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                ControlTemplate template = (value as Control).Template;
+
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                StringBuilder sb = new StringBuilder();
+                XmlWriter writer = XmlWriter.Create(sb, settings);
+                XamlWriter.Save(template, writer);
+
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                return "<< Generating ControlTemplate Error " + ex.Message + " >>";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+
+}
