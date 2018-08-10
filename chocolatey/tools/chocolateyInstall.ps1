76250e256b42307ecfcde2b14ec3fce1a0d0c38a
Bastian Schmidt <batzen@gmx.org>
2018-08-10 20:23:04 +0200

    Updating creation of desktop shortcut

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,12 +1,6 @@
-try {
-  # Create a desktop link to snoop executable
-  $parentfolder = Split-Path -parent $MyInvocation.MyCommand.Definition
-  $target = Join-Path $parentfolder 'snoop.exe'
-  Install-ChocolateyDesktopLink $target
+# Create a desktop link to snoop executable
+$parentfolder = Split-Path -parent $MyInvocation.MyCommand.Definition
+$target = Join-Path $parentfolder 'snoop.exe'
 
-  # the following is all part of error handling
-  Write-ChocolateySuccess 'snoop'
-} catch {
-  Write-ChocolateyFailure 'snoop' "$($_.Exception.Message)"
-  throw
-}
+$desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+Install-ChocolateyShortcut -ShortcutFilePath (Join-Path $desktopPath "Snoop.lnk") -TargetPath $target
