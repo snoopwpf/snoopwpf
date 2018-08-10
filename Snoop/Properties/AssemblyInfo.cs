9f917de8d451b2c54971cceb278b875c8dc47618
Bastian Schmidt <batzen@gmx.org>
2018-08-10 20:14:32 +0200

    Increasing version to 3.0

--- Snoop/Properties/AssemblyInfo.cs
+++ Snoop/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.9.0.0")]
-[assembly: AssemblyFileVersion("2.9.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
