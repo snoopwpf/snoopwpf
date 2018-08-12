f0736b1913c9e70cd5a401c02fa10aa986788120
Bastian Schmidt <batzen@gmx.org>
2018-08-12 10:22:23 +0200

    Adding uninstall script to remove desktop shortcut

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -3,4 +3,5 @@ $parentfolder = Split-Path -parent $MyInvocation.MyCommand.Definition
 $target = Join-Path $parentfolder 'snoop.exe'
 
 $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
-Install-ChocolateyShortcut -ShortcutFilePath (Join-Path $desktopPath "Snoop.lnk") -TargetPath $target
+$shortcutFilePath = Join-Path $desktopPath "Snoop.lnk"
+Install-ChocolateyShortcut -ShortcutFilePath $shortcutFilePath -TargetPath $target
---
+++ chocolatey/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$desktopPath = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$shortcutFilePath = Join-Path $desktopPath "Snoop.lnk"
+
+if (Test-Path($shortcutFilePath)) {
+	Remove-Item $shortcutFilePath -ErrorAction SilentlyContinue
+}
