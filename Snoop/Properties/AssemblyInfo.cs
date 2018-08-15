ef5118bb06d1a17116d021c4b30fd6ede3d9c2b6
Bastian Schmidt <batzen@gmx.org>
2018-08-15 20:13:31 +0200

    Setting version to 2.10

--- Snoop/Properties/AssemblyInfo.cs
+++ Snoop/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("2.10.0.0")]
+[assembly: AssemblyFileVersion("2.10.0.0")]
