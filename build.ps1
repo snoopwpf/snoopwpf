382d490bcb2619815962678c81d24f9e8285e54a
Bastian Schmidt <batzen@gmx.org>
2018-08-16 19:25:20 +0200

    Adding build of msi to build script

--- build.ps1
+++ build.ps1
@@ -9,11 +9,17 @@ Param(
 
 $ErrorActionPreference = "Stop"
 
-if ($null -eq (Get-Command vswhere)) {
-    Write-Error "vswhere could not be found. To build this project you need vswhere. You can install vswhere using 'choco install vswhere -y'."
-}
+$packages = Join-Path $PSScriptRoot "packages"
 
-$path = vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+$vswhere = Join-Path $packages "vswhere/tools/vswhere.exe"
+$nuget = Join-Path $packages "nuget.commandline/tools/nuget.exe"
+$candle = Join-Path $packages "wix/tools/candle.exe"
+$light = Join-Path $packages "wix/tools/light.exe"
+
+"Restoring packages"
+& ./paket.cmd restore
+
+$path = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
 if ($path) {
     $msbuild = Join-Path $path 'MSBuild/15.0/Bin/MSBuild.exe'
 }
@@ -22,7 +28,7 @@ if ($null -eq $msbuild -or !(Test-Path $msbuild)) {
     Write-Error "MSBuild could not be found."
 }
 
-# Build solution
+"Building solution"
 & $msbuild Snoop.sln /property:Configuration=$Configuration /v:m /nologo
 
 if ($LASTEXITCODE -ne 0) {
@@ -30,10 +36,6 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 if ($Package) {
-    if ($null -eq (Get-Command nuget)) {
-        Write-Error "nuget could not be found. To package this project you need nuget. You can install nuget using 'choco install nuget -y'."
-    }
-
     $buildOutput = Join-Path $PSScriptRoot "build/$Configuration"
     $version = (Get-Item (Join-Path $buildOutput "snoop.exe")).VersionInfo.FileVersion
     $outputDirectory = Join-Path $PSScriptRoot "build/publish"
@@ -43,7 +45,7 @@ if ($Package) {
     New-Item (Join-Path $buildOutput "Snoop.exe.gui") -ErrorAction SilentlyContinue | Out-Null    
 
     "Creating chocolatey package for version $version"
-    &nuget pack "$(Join-Path $PSScriptRoot 'chocolatey\snoop.nuspec')" -Version $version -Properties Configuration=$Configuration -OutputDirectory "$outputDirectory" -NoPackageAnalysis
+    & $nuget pack "$(Join-Path $PSScriptRoot 'chocolatey\snoop.nuspec')" -Version $version -Properties Configuration=$Configuration -OutputDirectory "$outputDirectory" -NoPackageAnalysis
 
     if ($LASTEXITCODE -ne 0) {
         Write-Error "Creating chocolatey package failed."
@@ -53,4 +55,18 @@ if ($Package) {
     $zipOutput = (Join-Path $outputDirectory "Snoop.$version.zip")
     Remove-Item $zipOutput -ErrorAction SilentlyContinue
     Compress-Archive -Path $buildOutput\Scripts, $buildOutput\*.dll, $buildOutput\*.pdb, $buildOutput\*.exe, $buildOutput\*.config -DestinationPath $zipOutput
+
+    "Creating msi for version $version"
+    $msiOutput = Join-Path $outputDirectory "Snoop.$version.msi"
+    & $candle Snoop.wxs -ext WixUIExtension -o Intermediate/Snoop.wixobj -dProductVersion="$version" -nologo
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Candle failed."
+    }
+
+    & $light -out "$msioutput" -b "$buildOutput" Intermediate/Snoop.wixobj -ext WixUIExtension -dProductVersion=$version -nologo
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Light failed."
+    }
 }
