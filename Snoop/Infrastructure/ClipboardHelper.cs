f809dafae5c648e650a1c583b39db1441721d70f
Bastian Schmidt <batzen@gmx.org>
2018-12-21 16:17:11 +0100

    Fixes #56 by using Clipboard from Windows Forms instead of WPF
    The Windows Forms Clipboard has retry logic which is not present in WPF

---
+++ Snoop/Infrastructure/ClipboardHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Snoop.Infrastructure
+{
+    using System;
+    using System.Windows.Forms;
+
+    public static class ClipboardHelper
+    {
+        public static void SetText(string text)
+        {
+            try
+            {
+                Clipboard.SetText(text, TextDataFormat.UnicodeText);
+            }
+            catch (Exception exception)
+            {
+                ErrorDialog.ShowExceptionMessageBox(exception, "Error copying to clipboard", "There was an error copying to the clipboard.\nPlease copy the error details from the above textbox manually.");
+            }
+        }
+    }
+}
