9c7154781a649cb2066887d722e865b5433945a6
Bastian Schmidt <batzen@gmx.org>
2019-10-06 15:06:35 +0200

    Removing unused file

--- Untitled1.ps1
+++
@@ -1,4 +0,0 @@
-﻿&"C:\DEV\OSS_Own\snoopwpf\bin\Debug\Snoop.exe"
-&"C:\DEV\OSS_Own\NativeToManagedTest\bin\Debug\x64\netcoreapp3.0\WPFTestApp.exe"
-
-#"C:\DEV\OSS_Own\snoopwpf\packages\wix\tools\heat.exe" dir . -o test.wxs -scom -sreg -svb6 -gg -suid -srd -sfrag
