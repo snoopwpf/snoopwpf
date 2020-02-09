3e13b71550712a7cbdf6534dd5e576beac21975d
Bastian Schmidt <batzen@gmx.org>
2020-02-09 11:44:59 +0100

    First try with commandline for snoop

--- Snoop/Program.cs
+++ Snoop/Program.cs
@@ -1,14 +1,73 @@
 ﻿namespace Snoop
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Windows;
+    using CommandLine;
 
     public static class Program
     {
+        public static bool Debug { get; set; }
+
         [STAThread]
         public static int Main(string[] args)
         {
+            var helpWriter = new StringWriter();
+
+            var parser = new Parser(x => x.HelpWriter = helpWriter);
+
+            return parser.ParseArguments<InspectCommandLineOptions, MagnifyCommandLineOptions, SnoopCommandLineOptions>(args)
+                    .MapResult(
+                        (InspectCommandLineOptions options) => Inspect(options),
+                        (MagnifyCommandLineOptions options) => Magnify(options),
+                        (SnoopCommandLineOptions options) => Run(options),
+                        errs => ErrorHandler(args, errs.ToList(), helpWriter));
+        }
+
+        private static int Inspect(InspectCommandLineOptions options)
+        {
+            var processInfo = new ProcessInfo(options.TargetPID);
+            var result = processInfo.Snoop(new IntPtr(options.TargetHwnd));
+            return result.Success
+                ? 0
+                : 1;
+        }
+
+        private static int Magnify(MagnifyCommandLineOptions options)
+        {
+            var processInfo = new ProcessInfo(options.TargetPID);
+            var result = processInfo.Magnify(new IntPtr(options.TargetHwnd));
+            return result.Success
+                ? 0
+                : 1;
+        }
+
+        private static int Run(SnoopCommandLineOptions options)
+        {
+            Debug = options.Debug;
+
             var app = new App();
             return app.Run();
+        }
+
+        private static int ErrorHandler(string[] args, IList<Error> errors, StringWriter helpWriter)
+        {
+            if (errors.Count == 1
+                && errors.All(x => x is NoVerbSelectedError))
+            {
+                return Run(new SnoopCommandLineOptions());
+            }
+
+            ShowErrorsGui(helpWriter.ToString());
+
+            return 1;
+        }
+
+        private static void ShowErrorsGui(string helpText)
+        {
+            MessageBox.Show(helpText, "Error while parsing commandline", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
