fec8a80da6a1d50c1d65b063bdce7f4904185870
Bastian Schmidt <batzen@gmx.org>
2020-02-09 13:26:24 +0100

    Adding sample UI automation driver

---
+++ TestHarnesses/SimpleUIAutomation.Application/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace SimpleUIAutomation.Application
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using System.Windows;
+
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TestButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.TestTextBox.AppendText(DateTime.Now.ToLongDateString() + Environment.NewLine);
+        }
+
+        private void StartAutomation_OnClick(object sender, RoutedEventArgs e)
+        {
+            var scriptLines = new[]
+            {
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Input Test-TextBox Foo",
+                "Input Test-TextBox Bar",
+                "Click Test-Button",
+                "Click Test-Button",
+                "Click Test-Button",
+            };
+
+            var tempFile = Path.GetTempFileName();
+            File.WriteAllLines(tempFile, scriptLines, Encoding.UTF8);
+
+            var automationAssemblyName = "SimpleUIAutomation.dll";
+            var automationAssemblyPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), automationAssemblyName);
+            var arguments = $"--targetPID {Process.GetCurrentProcess().Id} --assembly \"{automationAssemblyPath}\" --className SimpleUIAutomation.AutomationDriver --methodName StartAutomation --settingsFile \"{tempFile}\"";
+
+            Process.Start(@"..\..\..\..\bin\debug\Snoop.InjectorLauncher.x64.exe", arguments);
+        }
+    }
+}
