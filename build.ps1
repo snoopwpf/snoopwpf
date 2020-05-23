1223e88f89a7f071e32e819123a812b07af091ac
Bastian Schmidt <batzen@gmx.org>
2020-05-23 20:49:07 +0200

    Fixing build

--- build.ps1
+++ build.ps1
@@ -71,4 +71,4 @@ Write-Output "Microsoft (R) .NET Core SDK version $(& $env:DOTNET_EXE --version)
 Write-Output "Preparing and running build..."
 ExecSafe { & $env:DOTNET_EXE build $BuildProjectFile -c release /nodeReuse:false /nologo /consoleLoggerParameters:"ErrorsOnly;NoSummary" }
 Write-Output "Starting build..."
-ExecSafe { & $env:DOTNET_EXE run --project $BuildProjectFile --no-build -- $BuildArguments -nologo }
+ExecSafe { & $env:DOTNET_EXE run --project $BuildProjectFile --no-build -c release -- $BuildArguments -nologo }
