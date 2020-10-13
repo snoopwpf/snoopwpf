4b6d33837fe0f139ed33a144e37e2491f4540dea
Bastian Schmidt <batzen@gmx.org>
2020-10-13 19:34:41 +0200

    Adding dedicated methods/elements for retrieving binding errors

---
+++ Snoop.Core/Converters/IsNullOrEmptyStringConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Snoop.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class IsNullOrEmptyStringConverter : IValueConverter
+    {
+        public static readonly IsNullOrEmptyStringConverter DefaultInstance = new IsNullOrEmptyStringConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue)
+            {
+                return string.IsNullOrEmpty(stringValue);
+            }
+
+            return value is null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
