b64015cc4ddad64faf6760949dcbce7037d92cbd
Bastian Schmidt <batzen@gmx.org>
2022-03-20 08:22:11 +0100

    Only run DiffRunner if a debugger is attached

--- Snoop.Core.Tests/AssemblySetup.cs
+++ Snoop.Core.Tests/AssemblySetup.cs
@@ -1,5 +1,6 @@
 ﻿namespace Snoop.Core.Tests
 {
+    using System.Diagnostics;
     using System.Globalization;
     using NUnit.Framework;
 
@@ -13,6 +14,8 @@
             CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.CurrentCulture;
             CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.CurrentCulture;
+
+            DiffEngine.DiffRunner.Disabled = Debugger.IsAttached == false;
         }
     }
 }
