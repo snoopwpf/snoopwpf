85fde877b2f921a718a34f8b8abe64174e7b3f95
Bastian Schmidt <batzen@gmx.org>
2022-03-20 15:50:55 +0100

    Refactoring style retrieval

--- Snoop.Core/Converters/NullStyleConverter.cs
+++ Snoop.Core/Converters/NullStyleConverter.cs
@@ -4,6 +4,7 @@
     using System.Globalization;
     using System.Windows;
     using System.Windows.Data;
+    using Snoop.Infrastructure.Helpers;
 
     [ValueConversion(typeof(object), typeof(object))]
     [ValueConversion(typeof(object), typeof(Style))]
@@ -18,41 +19,17 @@
                 return value;
             }
 
-            // If the target does not have an explicit style, try to find the default style
+            return parameter switch
             {
-                if (parameter is FrameworkElement fe)
-                {
-                    var defaultStyleKey = FrameworkElementDefaultStyleKeyHelper.GetDefaultStyleKey(fe) ?? fe.GetType();
-                    return fe.TryFindResource(defaultStyleKey) as Style;
-                }
-
-                if (parameter is FrameworkContentElement fce)
-                {
-                    var defaultStyleKey = FrameworkContentElementDefaultStyleKeyHelper.GetDefaultStyleKey(fce) ?? fce.GetType();
-                    return fce.TryFindResource(defaultStyleKey) as Style;
-                }
-            }
-
-            return null;
+                FrameworkElement fe => FrameworkElementHelper.GetStyle(fe),
+                FrameworkContentElement fce => FrameworkElementHelper.GetStyle(fce),
+                _ => null
+            };
         }
 
         public object ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
         {
             return Binding.DoNothing;
         }
-
-#pragma warning disable CA1812
-
-        private class FrameworkElementDefaultStyleKeyHelper : FrameworkElement
-        {
-            public static object? GetDefaultStyleKey(FrameworkElement element) => element.GetValue(DefaultStyleKeyProperty);
-        }
-
-        private class FrameworkContentElementDefaultStyleKeyHelper : FrameworkElement
-        {
-            public static object? GetDefaultStyleKey(FrameworkContentElement element) => element.GetValue(DefaultStyleKeyProperty);
-        }
-
-#pragma warning restore CA1812
     }
 }
---
+++ Snoop.Core/Infrastructure/Helpers/FrameworkElementHelper.cs
@@ -0,0 +1,52 @@
+﻿namespace Snoop.Infrastructure.Helpers;
+
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+public static class FrameworkElementHelper
+{
+    private static readonly PropertyInfo frameworkElementThemeStylePropertyInfo = typeof(FrameworkElement).GetProperty("ThemeStyle", BindingFlags.Instance | BindingFlags.NonPublic)!;
+    private static readonly PropertyInfo frameworkContentElementThemeStylePropertyInfo = typeof(FrameworkContentElement).GetProperty("ThemeStyle", BindingFlags.Instance | BindingFlags.NonPublic)!;
+
+    public static Style? GetStyle(FrameworkElement fe)
+    {
+        var defaultStyleKey = FrameworkElementDefaultStyleKeyHelper.GetDefaultStyleKey(fe) ?? fe.GetType();
+        return fe.TryFindResource(defaultStyleKey) as Style;
+    }
+
+    public static Style? GetStyle(FrameworkContentElement fce)
+    {
+        var defaultStyleKey = FrameworkContentElementDefaultStyleKeyHelper.GetDefaultStyleKey(fce) ?? fce.GetType();
+        return fce.TryFindResource(defaultStyleKey) as Style;
+    }
+
+    public static FrameworkTemplate? GetTemplate(FrameworkElement fe)
+    {
+        return (FrameworkTemplate?)fe.GetType().GetProperty("TemplateInternal", BindingFlags.Instance | BindingFlags.NonPublic)!.GetValue(fe);
+    }
+
+    public static Style? GetThemeStyle(FrameworkElement fe)
+    {
+        return (Style?)frameworkElementThemeStylePropertyInfo.GetValue(fe);
+    }
+
+    public static Style? GetThemeStyle(FrameworkContentElement fce)
+    {
+        return (Style?)frameworkContentElementThemeStylePropertyInfo.GetValue(fce);
+    }
+
+#pragma warning disable CA1812
+
+    private class FrameworkElementDefaultStyleKeyHelper : FrameworkElement
+    {
+        public static object? GetDefaultStyleKey(FrameworkElement element) => element.GetValue(DefaultStyleKeyProperty);
+    }
+
+    private class FrameworkContentElementDefaultStyleKeyHelper : FrameworkElement
+    {
+        public static object? GetDefaultStyleKey(FrameworkContentElement element) => element.GetValue(DefaultStyleKeyProperty);
+    }
+
+#pragma warning restore CA1812
+}
