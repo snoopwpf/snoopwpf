15aaed46ce51c9cf9d6b6977b67689c8ec388706
Philipp Zimmermann <9058813+Garzuuhl@users.noreply.github.com>
2024-05-29 13:42:04 +0200

    Fix brush binding errors

---
+++ Snoop.Core/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace Snoop.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+[ValueConversion(typeof(Color), typeof(SolidColorBrush))]
+public sealed class ColorToSolidColorBrushConverter : IValueConverter
+{
+  public static ColorToSolidColorBrushConverter DefaultInstance { get; } = new();
+
+  public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not Color color)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        var brush = new SolidColorBrush(color);
+        brush.Freeze();
+        return brush;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not SolidColorBrush brush)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        return brush.Color;
+    }
+}
